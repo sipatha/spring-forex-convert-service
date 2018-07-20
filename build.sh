7a121fab81c869f542691c68d98c33255fcfdf48
n002213f <dev@shoko.co.za>
2018-07-20 08:13:54 +0200

    - force delete dangling images

--- build.sh
+++ build.sh
@@ -2,5 +2,5 @@
 # docker ps -a -q --filter="name=forex-convert-service"
 mvn clean install
 docker build --build-arg JAR_FILE=target/forex-convert-service-0.0.1-SNAPSHOT.jar --file=Dockerfile --tag=spring-demo/forex-convert-service:latest .
-docker rmi $(docker images -f 'dangling=true' -q)
+docker rmi $(docker images -f 'dangling=true' -q) -f
 # docker run --network=spring-demo -i -t -d spring-demo/forex-convert-service
