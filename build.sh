a831c771df0b52e680f7237949c4df2a81a1c569
n002213f <dev@shoko.co.za>
2018-07-20 07:51:26 +0200

    - don;t start the container - remove dangling images

--- build.sh
+++ build.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
-docker ps -a -q --filter="name=forex-convert-service"
+# docker ps -a -q --filter="name=forex-convert-service"
 mvn clean install
 docker build --build-arg JAR_FILE=target/forex-convert-service-0.0.1-SNAPSHOT.jar --file=Dockerfile --tag=spring-demo/forex-convert-service:latest .
-docker run --network=spring-demo -i -t  -d spring-demo/forex-convert-service
+docker rmi $(docker images -f 'dangling=true' -q)
+# docker run --network=spring-demo -i -t  -d spring-demo/forex-convert-service
